b4da90ac784cc4cd30113722b36d685305f1c682
QUEEN-YUSUF <queenyusuf2000@gmail.com>
2020-10-02 14:12:25 +0100

    initial commit

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ function diplay() {
     var x = document.getElementById("calculator");
       x.style.display = "flex";
   }
-
-  function calculate(){
-      var i = "";
+function calculate(){
+      var i;
       i = document.getElementById("name").value;
-      var j = 0;
-      j = document.getElementById("year").value;
-
+      var j;
+      j = document.getElementById("Age").value;
       j *= 365;
-    
-      document.getElementById("age").innerHTML = 
+
+      if(i === "" || j === "") {
+          alert("Fil in your details");
+      }
+      else{
+        document.getElementById("answer").innerHTML = i + "\t you have lived \t" + j + "\tdays";}
+      }
